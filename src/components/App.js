3f50fd5ea390d7d924cc9928dcfa100a46595b40
Muddhit Baid <61585314+Mukulbaid63@users.noreply.github.com>
2021-02-17 14:29:51 +0545

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -11,29 +11,7 @@ class App extends Component {
     { name: 'New York', country: 'USA',key:'location3' },
     { name: 'Darjeeling', country: 'India',key:'location4' },
     { name: 'Tokyo', country: 'Japan',key:'location5' },
-    { name: 'Lonavala', country: 'India',key:'location6' },
-    { name: 'Brandenburg Gate', country: 'Germany' ,key:'location7'},
-    { name: 'Reichstag Building', country: 'Germany',key:'location8' },
-    { name: 'Museum Island', country: 'Germany' ,key:'location9'},
-    { name: 'Munnar', country: 'India',key:'location10' },
-    { name: 'Leh Ladakh', country: 'India',key:'location11' },
-    { name: 'Goa', country: 'India',key:'location12' },
-    { name: 'Agra', country: 'India',key:'location13' },
-    { name: 'Dalhousie', country: 'India' ,key:'location14'},
-    { name: 'Coorg', country: 'India',key:'location15' },
-    { name: 'Rome', country: 'Italy',key:'location16' },
-    { name: 'Milan', country: 'Italy',key:'location17' },
-    { name: 'Venice', country: 'Italy' ,key:'location18'},
-    { name: 'Varanasai', country: 'India',key:'location19' },
-    { name: 'Jaipur', country: 'India' ,key:'location20'},
-    { name: 'The Hofburg', country: 'Austria',key:'location21' },
-    { name: 'Belvedere Palace', country: 'Austria',key:'location22' },
-    { name: 'St. Stephen Cathedral', country: 'Austria',key:'location23' },
-    { name: 'Kahna National Park', country: 'India',key:'location24' },
-    { name: 'Amritsar', country: 'India' ,key:'location25'},
-    { name: 'Mussoorie', country: 'India' ,key:'location26'},
-    { name: 'Mount Abu', country: 'India',key:'location27' },
-    { name: 'Tirupati', country: 'India' ,key:'location28'},
+    { name: 'Lonavala', country: 'India',key:'location6' }
     ]
     this.result = this.cityList.filter(word => word.country == 'India');
   }
@@ -45,7 +23,7 @@ class App extends Component {
       <div id="main">
         <ol>
         {this.result.map((x)=>(
-          <li key={x.key} >{x.name}({x.country})</li>
+          <li key={x.key} >{x.name}</li>
         ))}
         </ol>
       </div>
